b24c7e404c442e7d2a62662566967c7d51e6b761
jonas maratita <jonas.maratita@gmail.com>
2024-05-18 00:53:42 -0700

    #16 updated typescript

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,17 +1,16 @@
-var typed = new Typed('#typed', {
-    strings: [
-        'Financial Analyst',
-        'Cloud Engineer'
-    ],
-    typeSpeed: 50,
-    backSpeed: 50,
-    loop: true
-});
+// var typed = new Typed('#typed', {
+// strings: [
+////  'Financial Analyst'
+// ],
+// typeSpeed: 50,
+// backSpeed: 50,
+// loop: true
+//})
 
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("https://qx4i3eeoc6qh7jhnugieqi4bue0btyux.lambda-url.us-east-1.on.aws/");
-    let data = await response.json();
-    counter.innerHTML = ` Views: ${data}`;
+let response = await fetch("https://qx4i3eeoc6qh7jhnugieqi4bue0btyux.lambda-url.us-east-1.on.aws/");
+let data = await response.json();
+ counter.innerHTML = ` Views: ${data}`;
 }
 updateCounter()
